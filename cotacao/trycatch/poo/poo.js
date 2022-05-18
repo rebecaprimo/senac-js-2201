1c56422d0bdddb41995ff0b64f95086a57295658
REBECA RODRIGUES DE PRIMO <rebeca.rprimo@senacsp.edu.br>
2022-05-18 20:45:00 -0300

    final de aula

--- cotacao/trycatch/poo/poo.js
+++ cotacao/trycatch/poo/poo.js
@@ -29,15 +29,34 @@ user.listar();
 
 class Calculadora {
     constructor(){
-        document.getElementById('calc').addEventListener('click', this.calcula);
+        const operacao = document.getElementById('operacao');
+        try{
+            this.calcula(operacao);
+        }catch{
+            alert(e.message);
+        }
+        const operacao = document.getElementById('operacao').value;
+        this.calcula
+        //document.getElementById('calc').addEventListener('click', this.calcula);
     }
 
     calcula(){
         const n1 = document.getElementById('num1').value;
         const n2 = document.getElementById('num2').value;
 
-        console.log('NUMERO 1: ' + n1)
+        //console.log('NUMERO 1: ' + n1)
+        switch(operacao) {
+            case 'soma':
+                resultado = this.soma(n1, n2);
+                break;
+            case 'subtracao':
+                resultado = this.subtracao(n1, n2);
+                break;
+            default:
+                throw SyntaxError('operação não disponível');
+            }
 
+            this.exibeResultado(resultado);
         const resultado = this.soma(n1+n2);
     }
 
@@ -53,4 +72,10 @@ class Calculadora {
     }
 }
 
-new Calculadora; 
+document.getElementById('calc').addEventListener('click', function(e){
+    e.preventDefault();
+
+   const obj = new Calculadora; 
+});
+
+ 
